3953b0b3b843cc05d4632621ea4422da73147efd
H-sooyeon <wndqhr__@naver.com>
2025-12-17 15:56:54 +0900

    style: 메인 페이지 태그에 아이콘 추가

--- frontend/src/app/(main)/_components/SideFilterbar.tsx
+++ frontend/src/app/(main)/_components/SideFilterbar.tsx
@@ -3,26 +3,78 @@
 import Searchbar from '@/components/Searchbar';
 import TagButton from '@/components/TagButton';
 import SimpleMemo from './SimpleMemo';
-import { useState } from 'react';
+import {
+  Calendar,
+  Clapperboard,
+  Film,
+  LineSquiggle,
+  Music2,
+} from 'lucide-react';
 
-const CATEGORIES = [
-  { id: 'all', label: '전체' },
-  { id: 'theater', label: '연극' },
-  { id: 'musical', label: '뮤지컬' },
-  { id: 'concert', label: '일기/여행' },
-  { id: 'movie', label: '영화' },
-  { id: 'etc', label: '기타' },
-];
+// const CATEGORIES = [
+//   { id: 'all', label: '전체' },
+//   { id: 'theater', label: '연극' },
+//   { id: 'musical', label: '뮤지컬' },
+//   { id: 'concert', label: '일기/여행' },
+//   { id: 'movie', label: '영화' },
+//   { id: 'etc', label: '기타' },
+// ];
 
 export default function SideFilterbar() {
-  const [selectedCategory, setSelectedCategory] = useState('all');
+  // const [selectedCategory, setSelectedCategory] = useState('all');
 
   return (
     <div className="min-w-0 w-full h-full flex flex-col">
       <div className="h-full">
         <Searchbar placeholder="로그 검색하기" onCalendarClick={() => {}} />
         <div className="flex gap-2.5 mt-7 flex-wrap">
-          {CATEGORIES.map((category) => (
+          <TagButton
+            onClick={() => {}}
+            className="flex justify-center items-center gap-1"
+          >
+            <Clapperboard
+              size={16}
+              color="var(--itta-point)"
+              className="flex justify-center items-center gap-1"
+            />
+            연극
+          </TagButton>
+          <TagButton
+            onClick={() => {}}
+            className="flex justify-center items-center gap-1"
+          >
+            <Film size={16} color="var(--itta-point)" />
+            연극
+          </TagButton>
+          <TagButton
+            onClick={() => {}}
+            className="flex justify-center items-center gap-1"
+          >
+            <Music2 size={16} color="var(--itta-point)" />
+            뮤지컬
+          </TagButton>
+          <TagButton
+            onClick={() => {}}
+            className="flex justify-center items-center gap-1"
+          >
+            <Calendar size={16} color="var(--itta-point)" />
+            일기/여행
+          </TagButton>
+          <TagButton
+            onClick={() => {}}
+            className="flex justify-center items-center gap-1"
+          >
+            <Clapperboard size={16} color="var(--itta-point)" />
+            영화
+          </TagButton>
+          <TagButton
+            onClick={() => {}}
+            className="flex justify-center items-center gap-1"
+          >
+            <LineSquiggle size={16} color="var(--itta-point)" />
+            기타
+          </TagButton>
+          {/* {CATEGORIES.map((category) => (
             <TagButton
               key={category.id}
               onClick={() => setSelectedCategory(category.id)}
@@ -34,7 +86,7 @@ export default function SideFilterbar() {
             >
               {category.label}
             </TagButton>
-          ))}
+          ))} */}
         </div>
       </div>
 
