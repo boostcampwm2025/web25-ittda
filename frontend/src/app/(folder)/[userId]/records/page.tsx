697733207dc3307076baf21d0f539645d6571588
IENFI <myh020118@gmail.com>
2025-12-19 07:49:43 +0900

    feat: 나의 기록 화면에 플로팅 작성 버튼 추가 (#29)

--- frontend/src/app/(folder)/[userId]/records/page.tsx
+++ frontend/src/app/(folder)/[userId]/records/page.tsx
@@ -5,6 +5,7 @@ import { useRouter, usePathname } from 'next/navigation';
 import Header from '@/components/Header';
 import { useQuery } from '@tanstack/react-query';
 import { fetchPostList } from '@/lib/api/posts';
+import FloatingCreateButton from '@/components/FloatingCreateButton';
 
 export default function HomePage() {
   const router = useRouter();
@@ -21,22 +22,25 @@ export default function HomePage() {
   }
 
   return (
-    <div className="w-full bg-white pb-10 h-full relative pb-8">
-      <Header title="나의 기록 - 일기/여행" />
-      <div className={`absolute transition-transform duration-300 w-full`}>
-        <div className="flex flex-col h-full w-full overflow-y-auto">
-          {data.map((item) => (
-            <DiaryPostShort
-              post={item}
-              key={item.id}
-              onClick={() => {
-                router.push(`${pathname}/${item.id}`);
-              }}
-            />
-          ))}
-          <div className="absolute left-3.75 top-8 w-[1.5px] bottom-0 bg-itta-gray2 pointer-events-none" />
+    <>
+      <div className="w-full bg-white pb-10 h-full relative pb-8">
+        <Header title="나의 기록 - 일기/여행" />
+        <div className="absolute transition-transform duration-300 w-full">
+          <div className="flex flex-col h-full w-full overflow-y-auto">
+            {data.map((item) => (
+              <DiaryPostShort
+                post={item}
+                key={item.id}
+                onClick={() => {
+                  router.push(`${pathname}/${item.id}`);
+                }}
+              />
+            ))}
+            <div className="absolute left-3.75 top-8 w-[1.5px] bottom-0 bg-itta-gray2 pointer-events-none" />
+          </div>
         </div>
       </div>
-    </div>
+      <FloatingCreateButton />
+    </>
   );
 }
