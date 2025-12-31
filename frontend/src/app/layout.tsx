1dd2724e397fe0fda635acc4aa6e1cd240c4e2ee
H-sooyeon <wndqhr__@naver.com>
2025-12-31 20:18:33 +0900

    feat: 루트 layout 수정(#37)
    - 사이드바 제거
    - 수정된 디자인의 헤더와 네비게이션바 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         />
         <Providers>
           <MswLoader />
-          <div className="flex flex-col min-h-screen w-full mx-auto shadow-2xl max-w-4xl relative transition-colors duration-300 dark:bg-[#121212] dark:text-white bg-white text-itta-black">
+          <div className="flex flex-col min-h-screen max-w-4xl mx-auto shadow-2xl relative transition-colors duration-300 dark:bg-[#121212] dark:text-white bg-white text-itta-black">
             <ConditionalHeader />
             {children}
             <BottomNavigation />
