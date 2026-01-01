92461deb198f09d3dbb456e6ea86d46cd76fdffb
H-sooyeon <wndqhr__@naver.com>
2026-01-02 01:23:10 +0900

    chore: 데모용 이미지 도메인 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 // TODO: 나중에는 이미지 호스팅용 CDN 도메인으로 변경 필요
-const imageDomains = ['picsum.photos', 'catnews.net', 'cdn.ynenews.kr', 'mblogthumb-phinf.pstatic.net', 'i0.wp.com', 'velog.velcdn.com', 'biz.chosun.com'];
+const imageDomains = [
+  'picsum.photos',
+  'catnews.net',
+  'cdn.ynenews.kr',
+  'mblogthumb-phinf.pstatic.net',
+  'i0.wp.com',
+  'velog.velcdn.com',
+  'biz.chosun.com',
+  'images.unsplash.com',
+];
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -13,13 +22,13 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  
+
   images: {
     remotePatterns: imageDomains.map((host) => ({
       protocol: 'https',
       hostname: host,
     })),
-  }
+  },
 };
 
 export default nextConfig;
