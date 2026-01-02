5a50334044f8a960606a66b4dc8f6aa72225797a
H-sooyeon <wndqhr__@naver.com>
2026-01-02 15:50:13 +0900

    fix: plaiceholder 라이브러리는 서버 컴포넌트에서만 사용이 가능하므로 유틸 함수 분리
    - 이미지 로딩 시 blur 처리를 해주는 plaiceholder 라이브러리는 클라이언트 컴포넌트에서 import 할 수 없음

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,18 +1,7 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-import { getPlaiceholder } from 'plaiceholder';
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getBlurImg = async (imgSrc: string) => {
-  try {
-    const buffer = await fetch(imgSrc).then(async (res) =>
-      Buffer.from(await res.arrayBuffer()),
-    );
-    const { base64 } = await getPlaiceholder(buffer, { size: 10 });
-    return base64;
-  } catch {}
-};
