d4ab4472637d3bbbf777bbcb4d9ad807616d1817
H-sooyeon <wndqhr__@naver.com>
2026-01-02 15:41:35 +0900

    refactor: 블러로 빈 이미지 공간 이미지가 렌더링 될 때까지 표현(#37)
    - 동적 이미지의 경우(서버로부터 받아오는 이미지) getBlurImg 유틸 함수를 사용해 blurDataUrl을 얻어와야 함(base64 url)
    - 현재는 placeholder에 blur만 작성해주었으며, 서버로부터 데이터를 받아온 후 타입과 데이터 구조 변경 필요

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,8 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import { getPlaiceholder } from 'plaiceholder';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
