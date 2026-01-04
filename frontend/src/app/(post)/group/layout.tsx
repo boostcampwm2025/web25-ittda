f032ed0dbee2cf816d65970904c09076de0248ee
H-sooyeon <wndqhr__@naver.com>
2026-01-04 11:55:25 +0900

    style: 기록함 헤더 배경색 진하게 수정 및 pt 제거(#37)

--- frontend/src/app/(post)/group/layout.tsx
+++ frontend/src/app/(post)/group/layout.tsx
@@ -4,7 +4,7 @@ export default function GroupMonthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="p-6 space-y-8 pb-32 transition-colors duration-300 min-h-full dark:bg-[#121212] bg-[#FDFDFD]">
+    <div className="p-6 pt-0 space-y-8 pb-32 transition-colors duration-300 min-h-full dark:bg-[#121212] bg-[#FDFDFD]">
       {children}
     </div>
   );
