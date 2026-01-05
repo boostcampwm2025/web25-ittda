ed0fc5db0f7ae65861c30b090d7fcb7949b32949
openLeeWorld <leekipil98@gmail.com>
2026-01-05 21:09:35 +0900

    feat: main.ts에 AllExceptionFilter http, ws에 대해 적용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllHttpExceptionFilter } from './common/exception_filters/AllHttpExceptionFilter';
+import { AllWsExceptionFilter } from './common/exception_filters/AllWsExceptionFilter';
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api'); // 기본: /api/~
+
+  // 전역 예외 필터를 HTTP, WS에 대해 분리
+  // HTTP 전역 예외 필터 설정
+  app.useGlobalFilters(new AllHttpExceptionFilter());
+
+  // WS 전역 예외 필터 설정
+  app.useGlobalFilters(new AllWsExceptionFilter());
 
   await app.listen(process.env.PORT ?? 4000); // next랑 3000겹쳐서 4000함
 }
