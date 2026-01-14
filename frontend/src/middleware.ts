7b4f2bb96489803e3dcebcb25ff6312525def2db
H-sooyeon <wndqhr__@naver.com>
2026-01-14 14:09:08 +0900

    refactor: 로그인한 유저 accessToken으로 판별하던 것을 refreshToken으로 변경(#62)
    - accessToken은 메모리로 관리
    - 메모리로 관리되는 토큰은 새로고침시 사라지므로 새로고침 시에도 남아있는 refreshToken으로 판별

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,11 +6,11 @@ export function middleware(request: NextRequest) {
 
   // 로그인 페이지 접근 시
   if (pathname === '/login') {
-    // HttpOnly 쿠키에서 accessToken 확인
-    const accessToken = request.cookies.get('accessToken');
+    // HttpOnly 쿠키에서 refreshToken 확인
+    const refreshToken = request.cookies.get('refreshToken');
 
     // 로그인한 유저면 홈으로 리다이렉트
-    if (accessToken?.value) {
+    if (refreshToken?.value) {
       return NextResponse.redirect(new URL('/', request.url));
     }
   }
