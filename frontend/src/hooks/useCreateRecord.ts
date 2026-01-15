d28436d99971c62ebb2efbc95c1c2d63befc0ab3
Yeong-si <dtd1110@gmail.com>
2026-01-15 23:09:09 +0900

    chore: 임시 에러 처리 제거(#73)

--- frontend/src/hooks/useCreateRecord.ts
+++ frontend/src/hooks/useCreateRecord.ts
@@ -8,7 +8,7 @@ export const useCreateRecord = () => {
   const router = useRouter();
   const { userId } = useAuthStore();
   //TODO : 임시 로직 이후 확인 예정
-  if (!userId) throw new Error('로그인 필요');
+  //if (!userId) throw new Error('로그인 필요');
   const mutation = useApiPost<SuccessResponse<RecordDetail>>(
     '/posts',
     {
