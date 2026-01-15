5e7756b09537ac58fccdd2a4ba7d220d57b1abc2
Yeong-si <dtd1110@gmail.com>
2026-01-15 18:29:58 +0900

    fix: 기존 MSW 로 반환하던 rewirte 삭제 및 구조 수정(#73) - response 응답값 type 정의 후 설정 - MSW 로 들어가던 post 관련 호출 올바르게 백엔드 호출하도록 수정

--- frontend/src/hooks/useCreateRecord.ts
+++ frontend/src/hooks/useCreateRecord.ts
@@ -1,22 +1,22 @@
 import { useAuthStore } from '@/store/useAuthStore';
 import { useApiPost } from './useApi';
-import { CreateRecordRequest } from '@/lib/types/record';
 import { useRouter } from 'next/navigation';
+import { SuccessResponse } from '@/lib/types/response';
+import { RecordDetail } from '@/lib/types/recordResponse';
 export const useCreateRecord = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const router = useRouter();
   const { userId } = useAuthStore();
-
   //TODO : 임시 로직 이후 확인 예정
-  //if (!userId) throw new Error('로그인 필요');
-  const mutation = useApiPost<CreateRecordRequest>(
-    '/api/posts',
+  if (!userId) throw new Error('로그인 필요');
+  const mutation = useApiPost<SuccessResponse<RecordDetail>>(
+    '/posts',
     {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       onSuccess: (data) => {
         // TODO: 응답 형태 확인 후 주석 해제
-        // if (data?.id) {
-        //   router.push(`/record/${data.id}`);
+        // if (res.success && res.data?.data.id) {
+        //   router.push(`/record/${res.data.data.id}`);
         // }
       },
     },
