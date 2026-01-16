549aa482b6690d2d1772e89beccca39c0b1adbba
Yeong-si <dtd1110@gmail.com>
2026-01-16 09:56:42 +0900

    feat: 게시글 생성 후 기록 상세보기로 이동(#73)

--- frontend/src/hooks/useCreateRecord.ts
+++ frontend/src/hooks/useCreateRecord.ts
@@ -4,20 +4,17 @@ import { useRouter } from 'next/navigation';
 import { SuccessResponse } from '@/lib/types/response';
 import { RecordDetail } from '@/lib/types/recordResponse';
 export const useCreateRecord = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const router = useRouter();
   const { userId } = useAuthStore();
   //TODO : 임시 로직 이후 확인 예정
   //if (!userId) throw new Error('로그인 필요');
-  const mutation = useApiPost<SuccessResponse<RecordDetail>>(
-    '/posts',
+  const mutation = useApiPost<RecordDetail>(
+    '/api/posts',
     {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      onSuccess: (data) => {
-        // TODO: 응답 형태 확인 후 주석 해제
-        // if (res.success && res.data?.data.id) {
-        //   router.push(`/record/${res.data.data.id}`);
-        // }
+      onSuccess: (res) => {
+        if (res.success && res.data?.id) {
+          router.push(`/record/${res.data?.id}`);
+        }
       },
     },
     false,
