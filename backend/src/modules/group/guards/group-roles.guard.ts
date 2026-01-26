6afe49fb7fb7212ac421fe6277de6f79b35fde9c
openLeeWorld <leekipil98@gmail.com>
2026-01-26 19:33:02 +0900

    fix: group-record에서 조회 api에 그룹 가드 적용 및 그룹 가드를 일치에서 최소 권한 이상으로 변경

--- backend/src/modules/group/guards/group-roles.guard.ts
+++ backend/src/modules/group/guards/group-roles.guard.ts
@@ -2,11 +2,19 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GROUP_ROLE_KEY } from './group-roles.decorator';
 import { GroupService } from '../group.service';
+import { GroupRoleEnum } from '@/enums/group-role.enum';
 
 import type { Request } from 'express';
 import type { MyJwtPayload } from '../../auth/auth.type';
 
 type RequestWithUser = Request & { user?: MyJwtPayload };
+
+// 역할 우선순위 정의
+const rolePriority: Record<GroupRoleEnum, number> = {
+  VIEWER: 1,
+  EDITOR: 2,
+  ADMIN: 3,
+};
 
 @Injectable()
 export class GroupRoleGuard implements CanActivate {
@@ -16,7 +24,7 @@ export class GroupRoleGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+    const requiredRoles = this.reflector.getAllAndOverride<GroupRoleEnum[]>(
       GROUP_ROLE_KEY,
       [context.getHandler(), context.getClass()],
     );
@@ -25,19 +33,17 @@ export class GroupRoleGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest<RequestWithUser>();
     const user = request.user;
-
-    if (!user || typeof user !== 'object') {
-      return false;
-    }
+    if (!user || typeof user !== 'object') return false;
 
     const userId = user.sub;
     const groupId = request.params.groupId;
 
     const member = await this.groupService.findMember(userId, groupId);
-    // TODO: 서비스 규모가 커지면 이 권한 정보를 Redis 등에 캐싱 고려
-
     if (!member) return false;
 
-    return requiredRoles.includes(member.role);
+    // 최소 요구되는 역할 중 하나라도 만족하면 true
+    return requiredRoles.some(
+      (required) => rolePriority[member.role] >= rolePriority[required],
+    );
   }
 }
