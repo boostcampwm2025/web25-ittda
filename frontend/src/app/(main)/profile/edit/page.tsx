7431ae6c55056dabac0e45eb4c5aa002b41ea927
H-sooyeon <wndqhr__@naver.com>
2026-01-27 21:34:36 +0900

    feat: 마이페이지 프로필 수정 유저 정보 조회 후 데이터 적용

--- frontend/src/app/(main)/profile/edit/page.tsx
+++ frontend/src/app/(main)/profile/edit/page.tsx
@@ -1,16 +1,20 @@
+import { QueryClient } from '@tanstack/react-query';
 import ProfileEditClient from './_components/ProfileEditClient';
+import { getCachedUserProfile } from '@/lib/api/profile';
 
-const profile = {
-  id: 'uuid',
-  profileImageId: '/profile-ex.jpeg',
-  nickname: '도비',
-  email: 'wndqhr__@naver.com',
-};
+export default async function ProfileEditPage() {
+  const queryClient = new QueryClient();
 
-export default function ProfileEditPage() {
+  if (process.env.NEXT_PUBLIC_MOCK !== 'true') {
+    const profile = await getCachedUserProfile();
+
+    // QueryClient에 직접 넣어서 HydrationBoundary로 클라이언트에 전달
+    queryClient.setQueryData(['profile', 'me'], profile);
+  }
+
   return (
     <div className="w-full flex flex-col min-h-screen dark:bg-[#121212] dark:text-white bg-white text-itta-black">
-      <ProfileEditClient profile={profile} />
+      <ProfileEditClient />
     </div>
   );
 }
