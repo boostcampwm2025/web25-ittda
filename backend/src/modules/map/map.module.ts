9b852dbb2e686695a63201f9880da5b23aad35e9
IENFI <myh020118@gmail.com>
2026-02-02 16:31:42 +0900

    fix: MapModule에 PostBlock 엔티티 등록 (#188)
    - MapService에서 PostBlockRepository 주입 실패하던 문제를 해결
    - TypeOrmModule.forFeature에 PostBlock 추가

--- backend/src/modules/map/map.module.ts
+++ backend/src/modules/map/map.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from '@/modules/post/entity/post.entity';
 import { PostContributor } from '@/modules/post/entity/post-contributor.entity';
+import { PostBlock } from '@/modules/post/entity/post-block.entity';
 import { MapService } from './map.service';
 import { MapController } from './map.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, PostContributor])],
+  imports: [TypeOrmModule.forFeature([Post, PostContributor, PostBlock])],
   controllers: [MapController],
   providers: [MapService],
   exports: [MapService],
