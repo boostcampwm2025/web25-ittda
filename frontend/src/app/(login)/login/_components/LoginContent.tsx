4f5ca00b2116fea47ae1cf15f4574789ed5f1a59
Yeong-si <dtd1110@gmail.com>
2026-02-04 14:08:33 +0900

    fix: 그룹 초대 시 그룹 이름 undefined 임시 지움(#211)

--- frontend/src/app/(login)/login/_components/LoginContent.tsx
+++ frontend/src/app/(login)/login/_components/LoginContent.tsx
@@ -56,10 +56,10 @@ export default function LoginContent({
               {
                 onSuccess: (response) => {
                   const groupId = response.data.groupId;
-                  const groupName = response.data.group.name;
+                  //const groupName = response.data.group.name;
                   if (!groupId) createApiError(response);
                   deleteCookie('invite-code');
-                  toast.success(`${groupName} 그룹에 참여되었습니다!`);
+                  toast.success(`그룹에 참여되었습니다!`);
                   router.replace(`/group/${groupId}`);
                 },
               },
