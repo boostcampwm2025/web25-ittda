64a5edf29b2b82801a7eb8c55b5889d338f57028
openLeeWorld <leekipil98@gmail.com>
2026-02-05 10:54:33 +0900

    fix: pr 리뷰 반영 수정

--- backend/src/modules/auth/guards/kakao-auth.guard.ts
+++ backend/src/modules/auth/guards/kakao-auth.guard.ts
@@ -5,8 +5,8 @@ import { AuthGuard } from '@nestjs/passport';
 export class KakaoAuthGuard extends AuthGuard('kakao') {
   getAuthenticateOptions() {
     return {
-      //scope: ['profile_nickname', 'account_email'], // TODO: 이메일은 사업 출시하고 개인정보 동의항목 권한 신청해야 함
-      scope: ['profile_nickname'],
+      scope: ['profile_nickname', 'account_email'],
+      //scope: ['profile_nickname'],
     };
   }
 }
