a0238675a50830edabd398b901c66506390f272f
H-sooyeon <wndqhr__@naver.com>
2026-02-26 15:09:02 +0900

    fix: pupeteer 타임아웃 늘리고 에러 처리 추가

--- frontend/lighthouse.js
+++ frontend/lighthouse.js
@@ -5,7 +5,7 @@ module.exports = {
       // 측정 전 게스트 로그인 자동화
       puppeteerScript: './frontend/lighthouse.puppeteer.js',
       url: [
-        'http://localhost:3000/login', // 로그인 (public)
+        // 'http://localhost:3000/login', // 로그인 (public)
         'http://localhost:3000/', // 홈
         'http://localhost:3000/profile', // 프로필
         'http://localhost:3000/search', // 검색
