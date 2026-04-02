39f995438b40da4c63ccba539a61a9dc4b459759
H-sooyeon <wndqhr__@naver.com>
2026-04-02 13:43:47 +0900

    feat: 안드로이드 루트에서 뒤로가기 시 앱 종료 상태 체크

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,6 +15,7 @@ import { Suspense } from 'react';
 import StatusBarCover from '@/components/StatusBarCover';
 import NativeStatusBarSync from '@/components/NativeStatusBarSync';
 import NetworkGuard from '@/components/NetworkGuard';
+import AndroidBackHandler from '@/components/AndroidBackHandler';
 import { GoogleAnalytics } from '@next/third-parties/google';
 
 const notoSans = Noto_Sans_KR({
@@ -234,6 +235,7 @@ export default function RootLayout({
               <ThemeColorSetter />
               <NativeStatusBarSync />
               <NetworkGuard />
+              <AndroidBackHandler />
               <div data-app-root className="flex flex-col min-h-screen w-full mx-auto max-w-4xl relative transition-colors duration-300 dark:bg-[#121212] dark:text-white bg-white text-itta-black">
                 {/* status bar 커버: position fixed + 인라인 zIndex로 스크롤과 무관하게 항상 가림 */}
                 <StatusBarCover />
