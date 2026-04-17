515837d201e387c6020d15537c094e84d179180d
IENFI <myh020118@gmail.com>
2026-04-17 16:40:42 +0900

    refactor: 마이페이지 모듈에 refresh token 리포지토리 주입 추가 (#285)
    - 회원 탈퇴 시 refresh token 정리를 위해 RefreshToken 엔티티를 모듈에 등록
    - 마이페이지 서비스에서 refresh token 리포지토리를 사용할 수 있도록 의존성 구성 반영

--- backend/src/modules/mypage/mypage.module.ts
+++ backend/src/modules/mypage/mypage.module.ts
@@ -4,10 +4,11 @@ import { MyPageController } from './mypage.controller';
 import { MyPageService } from './mypage.service';
 import { User } from '../user/entity/user.entity';
 import { StatsModule } from '../stats/stats.module';
+import { RefreshToken } from '../auth/refresh_token/refresh_token.entity';
 
 // Mypage 모듈 정의
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), StatsModule],
+  imports: [TypeOrmModule.forFeature([User, RefreshToken]), StatsModule],
   controllers: [MyPageController],
   providers: [MyPageService],
   exports: [MyPageService],
