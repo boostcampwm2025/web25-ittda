fef9bda74bcd142a79c5f89c8f3947068fb13134
H-sooyeon <wndqhr__@naver.com>
2026-07-07 13:07:55 +0900

    feat: 푸시 알림 권한 요청 UX 추가(#287)

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
 
